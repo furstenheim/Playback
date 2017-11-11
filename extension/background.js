4cacf81e0631686a58eb68a7c2ff7a0b4332b162
gabriel <furstenheim@gmail.com>
2017-11-11 09:49:50 +0100

    Attach using debugging protocol

--- extension/background.js
+++ extension/background.js
@@ -1,6 +1,7 @@
 localStorage.debug = 'playback:*'
 const playback = require('../lib/playback')
 const CDP = require('chrome-remote-interface')
+const CDPE = require('../lib/extension-debugging-client')
 async function setUpCaching () {
   console.log('Starting caching')
   const tab = await new Promise(function (resolve, reject) {
@@ -21,7 +22,7 @@ async function setUpCaching () {
   // TODO err
   const target = targets.find(t => t.tabId === tab.id)
   console.log(targets)
-  const client = await new Promise(function (resolve, reject) {
+  /*const client = await new Promise(function (resolve, reject) {
     CDP({
       target: function (targets) {
         console.log(targets)
@@ -31,7 +32,13 @@ async function setUpCaching () {
         return remoteTarget
       }
     }, client => resolve(client))
+  })*/
+  const client = await new Promise(function (resolve, reject) {
+    CDPE({
+      tabId: tab.id
+    }, client => resolve(client))
   })
+
   const inspector = new playback.Interceptor({client})
   await inspector.init()
 }

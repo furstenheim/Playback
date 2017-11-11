8c4eaa6fcb4fd14cdc298a3e3f71453412b5138b
gabriel <furstenheim@gmail.com>
2017-11-11 11:36:05 +0100

    Clean client

--- extension/background.js
+++ extension/background.js
@@ -13,38 +13,41 @@ chrome.tabs.onUpdated.addListener(function (tabId, change, tab) {
 })
 
 async function onClick (tab) {
-  /*const tab = await new Promise(function (resolve, reject) {
-    chrome.tabs.query({
-      windowType: 'normal',
-      active: true,
-      lastFocusedWindow: true
-    }, function (tabs) {
-      resolve(tabs[0])
-    })
-  })*/
   debug('Selected tab', tab)
   if (activeClients[tab.id]) return cleanUpCaching(tab.id)
   setUpCaching(tab.id)
 }
 async function setUpCaching (tabId) {
-  debug('Starting caching')
-  activeClients[tabId] = {}
-  chrome.browserAction.setIcon({tabId, path: './resources/playback16.png'})
-  const client = await new Promise(function (resolve, reject) {
-    CDPE({
-      tabId: tabId
-    }, client => resolve(client))
-  })
+  try {
+    debug('Starting caching')
+    chrome.browserAction.setIcon({tabId, path: './resources/playback16.png'})
+    const client = await new Promise(function (resolve, reject) {
+      CDPE({
+        tabId: tabId
+      }, function (err, client) {
+        if (err) {
+          return reject(err)
+        }
+        resolve(client)
+      })
+    })
+    activeClients[tabId] = client
 
-  const inspector = new playback.Interceptor({client})
-  await inspector.init()
+    const inspector = new playback.Interceptor({client})
+    await inspector.init()
+  } catch (e) {
+    console.error(e)
+    const client = activeClients[tabId]
+    activeClients[tabId] = null
+    chrome.browserAction.setIcon({tabId, path: './resources/play16.png'})
+    if (client) client.clean()
+  }
 }
 
 async function cleanUpCaching (tabId) {
   debug('Clearing caching')
+  const client = activeClients[tabId]
   activeClients[tabId] = null
   chrome.browserAction.setIcon({tabId, path: './resources/play16.png'})
+  client.clean()
 }
-
-
-
